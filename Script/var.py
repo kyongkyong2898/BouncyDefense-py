2fd12cf3dc6647bca3a12b71b219bdb5875c7c95
kyongkyong2898 <kyongkyong2898@gmail.com>
2022-05-14 21:16:34 +0900

    123

--- Script/var.py
+++ Script/var.py
@@ -5,3 +5,13 @@ FPS = 40
 scene = 'title'
 state = ''
 menu = False
+
+class Battle():
+    player_energy = 0
+    player_energy_max = 0
+    player_life = 0
+    player_life_max = 0
+    
+
+class Save():
+    pass
